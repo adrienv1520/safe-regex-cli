156480df8e93b44879585bcddd2bd410c3839132
adrienvalcke <adrienvalcke@icloud.com>
2016-10-11 22:21:09 +0200

    created a directory for testing files with regex and recursive option

---
+++ test/regexdir/anotherdir/regex-file3.js
@@ -0,0 +1,7 @@
+console.log('regex-file3.js');
+
+var regexp = new RegExp('\\nabc', 'gimy');
+var reg = new RegExp('hellohello');
+
+var re = /(bb+bb+)+cc/;
+var re = /(hello+how are+)+you/;
---
+++ test/regexdir/regex-file1.js
@@ -0,0 +1,9 @@
+var re = /(b+b+)+c/;
+var r = /\w+.[a-z]/gi;
+
+console.log('hello');
+
+var regexp = new RegExp('\\nabc', 'gimy');
+var reg = new RegExp('hellohello');
+
+var re = /(bb+bb+)+cc/;
