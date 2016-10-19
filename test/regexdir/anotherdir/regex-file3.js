9cec8521489badebe03ef7d04ee7dad7dfff9173
adrienvalcke <adrienvalcke@icloud.com>
2016-10-19 20:40:04 +0200

    renamed test files

--- test/regexdir/anotherdir/regex-file3.js
+++
@@ -1,9 +0,0 @@
-console.log('regex-file3.js');
-
-var r1 = new RegExp('\\nabc', 'gimy');
-var r2 = new RegExp('hellohello');
-
-var r3 = /(bb+bb+)+cc/;
-var r4 = /(hello+how are+)+you/;
-
-var r5 = new RegExp ( '(ax+ax+)+ax');
