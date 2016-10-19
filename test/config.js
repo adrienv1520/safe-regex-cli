79acb30169d89cd6dbb72c800584137e6e2b6efc
adrienvalcke <adrienvalcke@icloud.com>
2016-10-19 20:43:14 +0200

    delete map to test unsafe regex

--- test/config.js
+++ test/config.js
@@ -7,14 +7,6 @@ const emptydir = path.join(__dirname, 'regexdir', 'emptydir');
 const phantompath = 'xgkskfdhflhg0965(rèlgjldfsdjdslfjslfl';
 const unsaferegex = /(safe-regex-cli+){10}/;
 
-// key = the regex | value = string data where the regex can be found
-let unsafemap = new Map();
-unsafemap.set('/(safe+){10}/', ' /(safe+){10}/ ');
-unsafemap.set('/(regex-cli+/]){10}/i', '//(regex-cli+){10}/i');
-unsafemap.set('/(cli+){10}/g', '  /(cli+){10}/g  ');
-unsafemap.set('/(safe-regex-cli+){10}/g', ' const myRegex = new RegExp("(safe-regex-cli+){10}", "g");');
-unsafemap.set('/(safe-regex-cli+){10}/gi', ' const myRegex = new RegExp ( "(safe-regex-cli+){10}","gi") ;');
-
 module.exports = {
   regexdir,
   unsafefile,
